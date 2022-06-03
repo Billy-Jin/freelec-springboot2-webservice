11dc0c689db930906c88326aba40b58831fcf5a4
Billy-Jin <uramjin@naver.com>
2022-06-03 22:14:03 +0900

    4장완료

--- src/main/java/com/uram/book/springboot/service/posts/PostsService.java
+++ src/main/java/com/uram/book/springboot/service/posts/PostsService.java
@@ -9,6 +9,9 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @RequiredArgsConstructor
 @Service
 public class PostsService {
@@ -35,6 +38,19 @@ public class PostsService {
                 .orElseThrow(()-> new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
 
         return new PostsResponseDto(entity);
+    }
+
+    @Transactional(readOnly = true)
+    public List<PostsResponseDto> findAllDesc() {
+        return postsRepository.findAllDesc().stream().map(PostsResponseDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @Transactional
+    public void delete(Long id) {
+        Posts posts = postsRepository.findById(id).orElseThrow(() ->
+                new IllegalArgumentException("해당 게시글이 없습니다. id="+id));
+        postsRepository.delete(posts);
     }
 }
 
--- src/main/java/com/uram/book/springboot/web/dto/PostsResponseDto.java
+++ src/main/java/com/uram/book/springboot/web/dto/PostsResponseDto.java
@@ -12,11 +12,13 @@ public class PostsResponseDto {
     private String title;
     private String author;
     private LocalDateTime modifiedDate;
+    private String content;
 
     public PostsResponseDto(Posts entity){
         this.id = entity.getId();
         this.title = entity.getTitle();
         this.author = entity.getAuthor();
         this.modifiedDate = entity.getModifiedDate();
+        this.content = entity.getContent();
     }
 }
